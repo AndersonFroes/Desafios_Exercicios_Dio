f098c59cd7930cb8c48cd8e3cba62c99cbf9321e
AndersonFroes <76731124+AndersonFroes@users.noreply.github.com>
2021-02-23 18:57:46 -0300

    Corrigido Desafio Bons ou Ruins

--- DesafiosJava/Conjuntos_Bons_ou_Ruins.java
+++ DesafiosJava/Conjuntos_Bons_ou_Ruins.java
@@ -1,49 +1,31 @@
 package DesafiosJava; // esse código você deve retirar para rodar na plataforma Dio
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Conjuntos_Bons_ou_Ruins {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N;
-        ArrayList<String> stringsSet = new ArrayList<>();
-        boolean isBadSet = false;
-        final String BAD_SET = "Conjunto Ruim";
-        final String GOOD_SET = "Conjunto Bom";
-        String conditionStr = GOOD_SET;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        while (true) {
-            N = Integer.parseInt(scanner.nextLine());
-            if (N == 0) {
-                break;
-            }
+        while (N != 0) {
+            List<String> conjunto = new ArrayList<>();
+            for (int i = 0; i < N; i++)
+                conjunto.add(br.readLine());
 
-            for (int i = 0; i < N; i++) {
-                stringsSet.add(scanner.nextLine().toLowerCase().trim());
-            }
+        conjunto = conjunto.stream().sorted().collect(Collectors.toList());
 
-            int i = 0, j = 0;
+        String saida = "Conjunto Bom";
+        for (int i = 0; i < N - 1; i++)
+            if (conjunto.get(i + 1).startsWith(conjunto.get(i)))
+                saida = "Conjunto Ruim";
 
-            for (i = 0; i < stringsSet.size(); i++) {
-                for (j = 0; j < stringsSet.size(); j++) {
-                    if (i != j) {
-                        if (stringsSet.get(i).startsWith(stringsSet.get(j))
-                                || stringsSet.get(j).startsWith(stringsSet.get(i))) {
-                            isBadSet = true;
-                            conditionStr = BAD_SET;
-                            break;
-                        }
-                    }
-                }
-                if (isBadSet) {
-                    break;
-                }
-            }
-            System.out.println(conditionStr);
-            stringsSet = new ArrayList<>();
-            conditionStr = GOOD_SET;
+        System.out.println(saida);
+        N = Integer.valueOf(br.readLine());
         }
-        scanner.close();
     }
 }
